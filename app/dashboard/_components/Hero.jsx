1522b09be575316420dcb1a237ebd28d831fcab9
SunkaraboinaPraveenKumar <praveenkumargitsunkaraboina@gmail.com>
2024-10-06 15:43:50 +0530

    home

--- app/dashboard/_components/Hero.jsx
+++ app/dashboard/_components/Hero.jsx
@@ -6,12 +6,12 @@ function Hero() {
             <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:items-center">
                 <div className="mx-auto max-w-xl text-center">
                     <h1 className="text-3xl font-extrabold sm:text-5xl">
-                        AI Short Video Generator
-                        <strong className="font-extrabold text-primary sm:block"> Custom Interview Paths, Powered by AI </strong>
+                        Student Attendance Manager
+                        <strong className="font-extrabold text-primary sm:block"> Custom Student Attendance Manager </strong>
                     </h1>
 
                     <p className="mt-4 sm:text-xl/relaxed">
-                        Unlock  AI-driven Video Creation. Use your ideas and develop and create a custom video .
+                        Unlock  the Web Based Attendance Manager with Visulization....
                     </p>
 
                     <div className="mt-8 flex flex-wrap justify-center gap-4">
