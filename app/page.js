67c653ef6b78be78ab984ea7f492e625894081d4
SunkaraboinaPraveenKumar <praveenkumargitsunkaraboina@gmail.com>
2024-10-06 14:39:49 +0530

    kinde

--- app/page.js
+++ app/page.js
@@ -1,16 +1,19 @@
 "use client"
-import { Button } from "@/components/ui/button";
-import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs";
+import { useEffect, useState } from "react";
 import { redirect } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(()=>{
-    redirect('/api/auth/login?post_login_redirect_url=/dashboard');
-  })
-  return (
-    <div>
-      
-    </div>
-  );
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (mounted) {
+      redirect('/api/auth/login?post_login_redirect_url=/dashboard');
+    }
+  }, [mounted]);
+
+  return <div />;
 }
